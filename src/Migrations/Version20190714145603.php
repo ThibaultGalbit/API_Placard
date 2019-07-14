b47a484605de4bc757bf9e611f6e5c16ab49854e
ThibaultGalbit <thibaultgalbit5@gmail.com>
2019-07-14 16:57:40 +0200

    ReTestCommit

---
+++ src/Migrations/Version20190714145603.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190714145603 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE documents (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, categorie VARCHAR(255) NOT NULL, number INT NOT NULL, creation_date DATETIME NOT NULL, update_date DATETIME NOT NULL, deleted_date DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('DROP TABLE document_test');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE document_test (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, name VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, categorie VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, number INT NOT NULL, creation_date DATETIME NOT NULL, update_date DATETIME NOT NULL, deleted_date DATETIME DEFAULT NULL, INDEX IDX_7FCBF29F9D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE document_test ADD CONSTRAINT FK_7FCBF29F9D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
+        $this->addSql('DROP TABLE documents');
+    }
+}
